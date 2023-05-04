c22154de23caed3a4350f7d06c0a2327e92e56ac
AhsanRandhawa <83049347+AhsanRandhawa@users.noreply.github.com>
2023-05-03 21:09:51 -0400

    Fixed teams Bug

--- app.R
+++ app.R
@@ -18,7 +18,7 @@ library(tidygraph)
 # load("LauwrangledData.RData")
 # load("MuhammadAhsanTahir.RData")
 # load("MichaelPerales.RData")
-DataWrangling<-load("/Users/lautencate/Documents/GitHub/blog2-TheBigThree/DataWrangling.RData")
+DataWrangling<-load("DataWrangling.RData")
 
 
 ###############################################
@@ -44,9 +44,9 @@ ui <- navbarPage(
   title = "Head to Head Premier League Games",
   theme = shinytheme("flatly"),
   
-  ##create network graph tab (Lau)
+  ##create network graph tab
   tabPanel(
-    title = "XXXX",
+    title = "Network Of Premier League",
     sidebarLayout(
       sidebarPanel(
         selectInput(
@@ -58,7 +58,7 @@ ui <- navbarPage(
       ),
       mainPanel(
         plotOutput(outputId = "net"),
-        "XXX"
+        ""
       )
     )
   ),
@@ -73,15 +73,22 @@ ui <- navbarPage(
   output$net <- renderPlot({
     set.seed(123)
     finalTablesolo <- finalTable %>%
-      filter(name1 == input$netvar)
+      filter(name1 == input$netvar | name2 == input$netvar) %>%
+      mutate(subjectTeam = input$netvar,
+             objectTeam = ifelse(name1 == input$netvar, name2, name1),
+             totalGoalDifference = ifelse(name1 == input$netvar, totalGoalDifference, -totalGoalDifference),
+             winDifference = ifelse(name1 == input$netvar, winDifference, -winDifference),
+             PositiveGoals = ifelse(totalGoalDifference > 0, "Positive Goal Difference",ifelse(totalGoalDifference == 0,"No Goal Difference","Negative Goal Difference")),
+             PositiveWins = ifelse(winDifference > 0, "Positive Win Difference",ifelse(winDifference == 0,"No Win Difference","Negative Win Difference"))) %>%
+      select(subjectTeam, objectTeam, totalGoalDifference, winDifference, PositiveGoals, PositiveWins)
     solo <- graph_from_data_frame(finalTablesolo, directed = FALSE)
     ggraph(solo, layout = "fr") +
       # ggnetwork() %>%
       # ggplot(aes(x = x, y = y, xend = xend, yend = yend)) +
-      geom_edge_link(aes(width = abs(totalGoalDifference), color = Goals),
+      geom_edge_link(aes(width = abs(totalGoalDifference), color = PositiveGoals),
                      arrow = arrow(type = "closed", length = unit(8, "pt"))) +
       scale_edge_width(name = "Goal Difference", range = c(0.5, 5), guide = guide_legend(title.position = "top", title.hjust = 0.5)) +
-      scale_color_manual(name = "", values = c("Positive Goal Difference" = "green", "Negative Goal Difference" = "red")) +
+      #scale_color_manual(name = "", values = c("Positive Goal Difference" = "green", "Negative Goal Difference" = "red", "No Goal Difference" = "yellow")) +
       geom_node_point() +
       geom_node_text(aes(label = name), repel = TRUE) +
       theme_void()
